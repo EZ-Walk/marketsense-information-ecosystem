4d43e4f000573e3a7b5c8697b1215a0abaa12217
EZ-Walk <rocketwalker@gmail.com>
2025-12-26 16:43:22 -0600

    Update build manifests and static chunks for improved page rendering
    - Reordered entries in app-build-manifest.json to ensure correct loading of page components.
    - Updated encryption key in server-reference-manifest.json for enhanced security.
    - Modified client-reference-manifest.js to reflect changes in module mappings and chunk identifiers.
    - Updated webpack hash in static chunks to reflect recent builds.
    - Adjusted layout and page JavaScript files to ensure proper exports and module handling.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ab4caa7022bcddc1"; }
+/******/ 		__webpack_require__.h = function() { return "b213de7d96e93ffd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
